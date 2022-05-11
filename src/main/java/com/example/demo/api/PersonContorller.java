a1196ac14eb2e67ab3b0e41d1e77f599da2a621b
juyoungpang <jenna990615@naver.com>
2022-05-11 12:40:15 +0900

    spring demo

---
+++ src/main/java/com/example/demo/api/PersonContorller.java
@@ -0,0 +1,43 @@
+package com.example.demo.api;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/person")
+@RestController
+public class PersonContorller {
+    
+    private final PersonService personService;
+
+    @Autowired
+    public PersonContorller(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id)
+                            .orElse(null);
+    }
+}
