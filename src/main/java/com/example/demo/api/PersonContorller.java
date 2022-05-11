b4da9a325362875d55a67f9acd779b128bda1bd5
juyoungpang <jenna990615@naver.com>
2022-05-11 13:04:40 +0900

    .

--- src/main/java/com/example/demo/api/PersonContorller.java
+++ src/main/java/com/example/demo/api/PersonContorller.java
@@ -7,6 +7,7 @@ import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +43,7 @@ public class PersonContorller {
             .orElse(null);
     }
 
-    @GetMapping(path = "{id}")
+    @DeleteMapping(path = "{id}")
     public void deletePersonById(@PathVariable("id") UUID id){
         personService.deletePerson(id);
     }
