8da2b9771885e4d2561037a27125164cdab0b38e
juyoungpang <jenna990615@naver.com>
2022-05-11 12:39:16 +0900

    spring boot demo

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,47 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+
+    private static List<Person> DB = new ArrayList<>();
+    
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+        
+}
